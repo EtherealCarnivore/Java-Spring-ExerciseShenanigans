ee3271ba9f4e16afea33eb82c8cdf434ebaaa61b
Konstantin@Manjaro-Linux <roughnight2@gmail.com>
2019-02-04 16:55:01 +0200

    Innit repo

---
+++ src/main/java/com/example/angiriscouncil/AngirisCouncilApplication.java
@@ -0,0 +1,14 @@
+package com.example.angiriscouncil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngirisCouncilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngirisCouncilApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/angiriscouncil/AngirisCouncilApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.angiriscouncil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngirisCouncilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
