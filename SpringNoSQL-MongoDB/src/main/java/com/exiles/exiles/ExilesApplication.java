621226e6f141732389b227e83c0e63d6ab085d33
Konstantin@Manjaro-Linux <roughnight2@gmail.com>
2019-02-09 14:07:40 +0200

    Innit MongoDB project

---
+++ SpringNoSQL-MongoDB/src/main/java/com/exiles/exiles/ExilesApplication.java
@@ -0,0 +1,14 @@
+package com.exiles.exiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExilesApplication.class, args);
+	}
+
+}
+
